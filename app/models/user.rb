7fa7cb098c55cdf9ebc23977f5f27a860025c320
Roberto Martínez Stocker <robertomstocker@gmail.com>
2016-01-28 19:21:59 -0300

    Persisted Facebook oauth token.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,22 @@ class User < ActiveRecord::Base
 
   has_many :user_answers
 
-
   def self.from_omniauth(auth)
-
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    user = where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user model has a name
       user.image = auth.info.image # assuming the user model has an image
     end
+
+    if auth.provider.eql? 'facebook'
+      user.facebook_token = auth.credentials.token
+      user.facebook_token_expiration = auth.credentials.expires_at
+    end
+
+    user.save
+    user
+
   end
 
   def get_score
