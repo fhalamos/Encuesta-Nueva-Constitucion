6d9163ac179675d5b61cd1ff915b7a50ba4b7cca
Roberto Martínez Stocker <robertomstocker@gmail.com>
2016-01-28 16:32:20 -0300

    Added Facebook token db persistence for querying friend list.

---
+++ db/migrate/20160128100325_add_facebook_token_expiration_to_users.rb
@@ -0,0 +1,5 @@
+class AddFacebookTokenExpirationToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :facebook_token_expiration, :datetime
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160117161319) do
+ActiveRecord::Schema.define(version: 20160128100325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,26 +59,28 @@ ActiveRecord::Schema.define(version: 20160117161319) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "email",                     default: "",    null: false
+    t.string   "encrypted_password",        default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",             default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
+    t.datetime "created_at",                                null: false
+    t.datetime "updated_at",                                null: false
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
     t.string   "image"
-    t.boolean  "admin",                  default: false
-    t.integer  "Xposition",              default: 0
-    t.integer  "Yposition",              default: 0
-    t.boolean  "famous",                 default: false
+    t.boolean  "admin",                     default: false
+    t.integer  "Xposition",                 default: 0
+    t.integer  "Yposition",                 default: 0
+    t.boolean  "famous",                    default: false
+    t.string   "facebook_token"
+    t.datetime "facebook_token_expiration"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
