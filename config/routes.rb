fd8d9b8879c1555483ed921e0f41560007735bad
Roberto Martínez Stocker <robertomstocker@gmail.com>
2016-01-28 16:35:43 -0300

    Removed unused routes.

--- config/routes.rb
+++ config/routes.rb
@@ -18,8 +18,9 @@ Rails.application.routes.draw do
   end
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks"}
-  get '/users/sign_out' => 'devise/sessions#destroy'
-  resources :users
+
+
+  #resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
