0cc6762b8a64a1fa65a43ec79f315dab66c8994f
Roberto Martínez Stocker <robertomstocker@gmail.com>
2016-02-28 17:34:00 -0300

    Responsive header menu. Responsive text font size.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -4,3 +4,10 @@ angular.module('encuestaNuevaConstitucion', [
 ]).config(['$httpProvider', function($httpProvider) {
   $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
 }]);
+
+$(document).ready(function(){
+  $('.main-nav-button').click(function(e){
+    $('#main-header .main-nav').slideToggle();
+    console.log('click!');
+  });
+});
